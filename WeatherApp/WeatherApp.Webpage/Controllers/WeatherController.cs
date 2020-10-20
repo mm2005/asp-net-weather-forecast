e648d77c0cd52388cbcda3f611ed82077a703af6
tompuljalmeg <tompuljalmeg@gmail.com>
2020-10-20 14:56:42 +0200

    routing does it work?

---
+++ WeatherApp/WeatherApp.Webpage/Controllers/WeatherController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using WeatherApp.Webpage.Services;
+
+namespace WeatherApp.Webpage.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly CurrentWeatherService _currentWeatherService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, CurrentWeatherService currentWeatherService)
+        {
+            _logger = logger;
+            _currentWeatherService = currentWeatherService;
+        }
+
+        [HttpGet("{city}")]
+        public string Get(string city)
+        {
+            return _currentWeatherService.GetCurrentWeather(city).ToString();
+        }
+
+
+    }
+}
--- WeatherApp/WeatherApp.Webpage/Services/CurrentWeatherService.cs
+++ WeatherApp/WeatherApp.Webpage/Services/CurrentWeatherService.cs
@@ -12,9 +12,8 @@ namespace WeatherApp.Webpage.Services
 {
     public class CurrentWeatherService
     {
-        string apiKey = "3c850b0463346d2fffad82b66d5eb561";
+        //string apiKey = "3c850b0463346d2fffad82b66d5eb561";
         //string url = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
-        string url = "https://api.openweathermap.org/data/2.5/weather?q=budapest&appid=3c850b0463346d2fffad82b66d5eb561&units=metric";
 
         public CurrentWeatherService(IWebHostEnvironment webHostEnvironment)
         {
@@ -23,8 +22,9 @@ namespace WeatherApp.Webpage.Services
 
         public IWebHostEnvironment WebHostEnvironment { get; }
 
-        public CurrentWeather GetCurrentWeather()
+        public CurrentWeather GetCurrentWeather(string city)
         {
+            string url = @"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=3c850b0463346d2fffad82b66d5eb561&units=metric";
             string jsonString = "";
 
             using (var client = new HttpClient())
